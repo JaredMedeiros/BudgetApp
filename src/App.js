257fbbbe6401b1fcd68c468a9cea3b193d2dc6ff
JaredMedeiros <97901520+JaredMedeiros@users.noreply.github.com>
2022-08-05 15:47:23 -0500

    made initial income and expense cards

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
-import React from 'react'
+import React from 'react';
+import { Grid } from '@material-ui/core';
+
+import Details from './components/Details/Details';
+import useStyles from './styles';
 
 const App = () => {
+    const classes = useStyles();
+
     return (
         <div>
-
+            <Grid className = {classes.grid} container spacing={0} alginItems="center" justify="center" styles = {{height: '100vh'}}>
+                <Grid item xs={12} sm={4}>
+                    <Details title="Income"/>
+                </Grid>
+                <Grid item xs={12} sm={4}>
+                    Main
+                </Grid>
+                <Grid item xs={12} sm={4}>
+                    <Details title="Expense" />
+                </Grid>
+            </Grid>
         </div>
     )
 }
