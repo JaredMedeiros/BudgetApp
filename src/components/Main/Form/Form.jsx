0591be8eca033e1431101bd92d5db243131f5200
JaredMedeiros <97901520+JaredMedeiros@users.noreply.github.com>
2022-08-09 22:24:18 -0500

    need to fix bug. subcategory and date are resetting after speech finishes

--- src/components/Main/Form/Form.jsx
+++ src/components/Main/Form/Form.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useContext } from 'react';
+import React, { useState, useContext, useEffect } from 'react';
 import { TextField, Typography, Grid, Button, FormControl, InputLabel, Select, MenuItem } from '@material-ui/core';
 import { ExpenseTrackerContext } from '../../../context/context';
 import { v4 as uuidv4 } from 'uuid';
@@ -7,6 +7,8 @@ import { useSpeechContext } from '@speechly/react-client'
 import formatDate from '../../../utils/formatDate';
 import useStyles from './styles';
 import { incomeCategories, expenseCategories } from '../../../constants/categories';
+import { SettingsInputComponentSharp } from '@material-ui/icons';
+
 
 const initialState = {
     amount: '',
@@ -17,16 +19,56 @@ const initialState = {
 
 const Form = () => {
     const classes = useStyles();
-    const [formData, setFromData] = useState(initialState);
+    const [formData, setFormData] = useState(initialState);
     const { addTransaction } = useContext(ExpenseTrackerContext);
-    const { segment } = useSpeechContext;
+    const { segment } = useSpeechContext();
 
     const createTransaction = () => {
         const transaction = { ...formData, amount: Number(formData.amount), id: uuidv4()}
 
         addTransaction(transaction);
-        setFromData(initialState);
+        setFormData(initialState);
     };
+
+    useEffect(() => {
+        if(segment) {
+            if(segment.intent.intent === 'add_expense') {
+                setFormData({ ...formData, type: 'Expense' });
+            } else if (segment.intent.intent === 'add_income') {
+                setFormData({ ...formData, type: 'Income' })
+            } else if(segment.isFinal && segment.intent.intent === "create_transaction") {
+                return createTransaction();
+            } else if(segment.isFinal && segment.intent.intent === "cancel_transaction") {
+                return setFormData(initialState)
+            }
+
+            segment.entities.forEach((e) => {
+                const category = `${e.value.charAt(0)}${e.value.slice(1).toLowerCase()}`
+
+                switch (e.type) {
+                    case 'amount':
+                        setFormData({ ...formData, amount: e.value });
+                        break;
+
+                    case 'category':
+                        if(incomeCategories.map((iC) => iC.type).includes(category)) {
+                            setFormData({ ...formData, type: 'Income', category: category });
+                        } else if(expenseCategories.map((iC) => iC.type).includes(category)) {
+                            setFormData({ ...formData, type: 'Expense', category: category });
+                        }
+                         
+                        break; 
+                        
+                    case 'date':
+                        setFormData({ ...formData, category: e.value }); 
+                        break;   
+
+                    default:
+                        break;    
+                }
+            })
+        }
+    }, [segment])
 
     const selectedCategories = formData.type === 'Income' ? incomeCategories : expenseCategories;
 
@@ -34,17 +76,13 @@ const Form = () => {
         <Grid container spacing={2}>
             <Grid item xs={12}>
                 <Typography align="center" variant="subtitle2" gutterBottom>
-                    { segment ? () => (
-                        <>
-                            {segment.words.map((w) => w.value).join(" ")}
-                        </>
-                    ) : null}
+                    { segment && segment.words.map((w) => w.value).join(" ")}
                 </Typography>
             </Grid>
             <Grid item xs={6}>
                 <FormControl fullWidth>
                     <InputLabel>Type</InputLabel>
-                    <Select value={formData.type} onChange={(e) => setFromData({ ...formData, type: e.target.value })}>
+                    <Select value={formData.type} onChange={(e) => setFormData({ ...formData, type: e.target.value })}>
                         <MenuItem value="Income">Income</MenuItem>
                         <MenuItem value="Expense">Expense</MenuItem>
                     </Select>
@@ -53,16 +91,16 @@ const Form = () => {
             <Grid item xs={6}>
                 <FormControl fullWidth>
                     <InputLabel>Category</InputLabel>
-                    <Select value={formData.category} onChange={(e) => setFromData({ ...formData, category: e.target.value })}>
+                    <Select value={formData.category} onChange={(e) => setFormData({ ...formData, category: e.target.value })}>
                         {selectedCategories.map((c) => <MenuItem key={c.type} value={c.type}>{c.type}</MenuItem>)}
                     </Select>
                 </FormControl>
             </Grid>
             <Grid item xs={6}>
-                <TextField type="number" label="Amount" fullWidth value={formData.amount} onChange={(e) => setFromData({ ...formData, amount: e.target.value })} />
+                <TextField type="number" label="Amount" fullWidth value={formData.amount} onChange={(e) => setFormData({ ...formData, amount: e.target.value })} />
             </Grid>
             <Grid item xs={6}>
-                <TextField type="dater" label="Date" fullWidth value={formData.date} onChange={(e) => setFromData({ ...formData, date: formatDate(e.target.value) })} />
+                <TextField type="dater" label="Date" fullWidth value={formData.date} onChange={(e) => setFormData({ ...formData, date: formatDate(e.target.value) })} />
             </Grid>
             <Button className={classes.button} variant="outlined" color="primary" fullWidth onClick={createTransaction}>Create</Button>
         </Grid>
