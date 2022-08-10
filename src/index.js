dca566f75a7a7535cc6e530569a68a405e681817
JaredMedeiros <97901520+JaredMedeiros@users.noreply.github.com>
2022-08-09 19:09:14 -0500

    added speechly button

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { SpeechProvider } from '@speechly/react-client'
 
 import { Provider } from './context/context';
 import App from './App'
 import './index.css';
 
 ReactDOM.render(
-    <Provider> 
-        <App />
-    </Provider>,   
+    <SpeechProvider appId= "c77cbd20-31e5-4b48-b024-2fc66be36cf7" language = "en-US">
+        <Provider> 
+            <App />
+        </Provider>
+    </SpeechProvider>,   
 document.getElementById('root'));
