0b6e7bb5bcf89f3796557298fa6527a100985100
JaredMedeiros <97901520+JaredMedeiros@users.noreply.github.com>
2022-08-09 19:31:09 -0500

    fixed dependency issues

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import App from './App'
 import './index.css';
 
 ReactDOM.render(
-    <SpeechProvider appId= "c77cbd20-31e5-4b48-b024-2fc66be36cf7" language = "en-US">
+    <SpeechProvider appId = "c77cbd20-31e5-4b48-b024-2fc66be36cf7" language = "en-US">
         <Provider> 
             <App />
         </Provider>
